ce230fb0f45450254b316dca627829915a8b0905
HiminaE <himinaev@yandex.ru>
2024-04-17 12:44:03 +0300

    add App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    id("java")
+    application
+    id ("checkstyle")
 }
-
+application { mainClass.set("hexlet.code.App")
+}
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
@@ -10,10 +12,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation(dependencyNotation = "org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+//tasks.test {
+    //useJUnitPlatform()
+//}
