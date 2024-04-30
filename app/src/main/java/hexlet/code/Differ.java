e8616eea6915a34bf8e2f120cb90e61130252f16
HiminaE <himinaev@yandex.ru>
2024-04-30 14:34:27 +0300

    add change Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -103,11 +103,34 @@ public class Differ {
     }
     public static String chooseFormat(List<Map<String, Object>> diff, String format) throws JsonProcessingException {
         String result = "";
-        if (format.equals("json") || format.equals("stylish")) {
+        if (format.equals("stylish")) {
+            result = stylishFormat(diff);
+        } else if (format.equals("json")) {
             result = jsonFormat(diff);
         }  else {
             throw new RuntimeException("Неверный формат: " + format);
         }
         return result;
     }
+    public static String stylishFormat(List<Map<String, Object>> diff) {
+        StringBuilder result = new StringBuilder("{\n");
+        for (var d : diff) {
+            if (d.get("type").equals("deleted")) {
+                result.append("  - ").append(d.get("key")).append(": ").append(d.get("newValue")).append("\n");
+            }
+            if (d.get("type").equals("added")) {
+                result.append("  + ").append(d.get("key")).append(": ").append(d.get("newValue")).append("\n");
+            }
+            if (d.get("type").equals("unchanged")) {
+                result.append("    ").append(d.get("key")).append(": ").append(d.get("newValue")).append("\n");
+            }
+            if (d.get("type").equals("changed")) {
+                result.append("  - ").append(d.get("key")).append(": ").append(d.get("oldValue")).append("\n");
+                result.append("  + ").append(d.get("key")).append(": ").append(d.get("newValue")).append("\n");
+            }
+        }
+        result.append("}");
+        return result.toString();
+    }
 }
+
