dfa0ffa058a0593d9eabc85b4ca359cc940ba92f
HiminaE <himinaev@yandex.ru>
2024-05-05 20:53:36 +0300

    correcting errors in tests

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -10,26 +10,26 @@ public class AppTest {
     @Test
     public void test1() throws Exception {
         var actual = Differ.generate("src/test/resources/test1.json", "test2.json");
-        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize();
+        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
 
     @Test
     public void test2() throws Exception {
         var actual = Differ.generate("src/test/resources/test1.json", "test2.json", "stylish");
-        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize();
+        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
     public void test3() throws Exception {
         var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml", "plain");
-        String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize();
+        String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
     public void test4() throws Exception {
         var actual = Differ.generate("src/test/resources/test3.json", "test4.json", "plain");
-        String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize();
+        String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
 
