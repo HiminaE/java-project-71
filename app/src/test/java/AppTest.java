0a4c8be7bf230655a41b2172ebaafb1c514ce350
HiminaE <himinaev@yandex.ru>
2024-05-06 22:11:44 +0300

    Corrected Tests

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -8,26 +8,38 @@ import java.nio.file.Paths;
 
 public class AppTest {
     @Test
-    public void test1() throws Exception {
+    public void testJson() throws Exception {
         var actual = Differ.generate("src/test/resources/test1.json", "test2.json");
+        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testYml() throws Exception {
+        var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml");
         String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
 
     @Test
-    public void test2() throws Exception {
+    public void testJsonStylish() throws Exception {
         var actual = Differ.generate("src/test/resources/test1.json", "test2.json", "stylish");
         String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
-    public void test3() throws Exception {
+    public void testYmlPlain() throws Exception {
         var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml", "plain");
         String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
-    public void test4() throws Exception {
+    public void testYmlStylish() throws Exception {
+        var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml", "stylish");
+        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testJsonPlain() throws Exception {
         var actual = Differ.generate("src/test/resources/test3.json", "test4.json", "plain");
         String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
