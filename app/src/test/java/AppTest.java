be65aba43c2f635491f9460e96a3eb38c598fd14
HiminaE <himinaev@yandex.ru>
2024-05-09 09:24:19 +0300

    Corrected resultFiles

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -9,41 +9,52 @@ import java.nio.file.Paths;
 public class AppTest {
     @Test
     public void testJson() throws Exception {
-        var actual = Differ.generate("src/test/resources/test1.json", "test2.json");
+        var actual = Differ.generate("src/test/resources/test3.json", "src/test/resources/test4.json");
         String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
     public void testYml() throws Exception {
-        var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml");
+        var actual = Differ.generate("src/test/resources/test1.yml", "src/test/resources/test2.yml");
         String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
 
     @Test
     public void testJsonStylish() throws Exception {
-        var actual = Differ.generate("src/test/resources/test1.json", "test2.json", "stylish");
+        var actual = Differ.generate("src/test/resources/test3.json", "src/test/resources/test4.json", "stylish");
+        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testYmlStylish() throws Exception {
+        var actual = Differ.generate("src/test/resources/test1.yml", "src/test/resources/test2.yml", "stylish");
         String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
     public void testYmlPlain() throws Exception {
-        var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml", "plain");
-        String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize());
-        assertEquals(expected, actual);
-    }
-    @Test
-    public void testYmlStylish() throws Exception {
-        var actual = Differ.generate("src/test/resources/test1.yml", "test2.yml", "stylish");
-        String expected = Files.readString(Paths.get("src/test/resources/outputStylish.txt").toAbsolutePath().normalize());
+        var actual = Differ.generate("src/test/resources/test1.yml", "src/test/resources/test2.yml", "plain");
+        String expected = Files.readString(Paths.get("src/test/resources/outputPlain.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
     @Test
     public void testJsonPlain() throws Exception {
-        var actual = Differ.generate("src/test/resources/test3.json", "test4.json", "plain");
-        String expected = Files.readString(Paths.get("src/test/resources/outputYml.txt").toAbsolutePath().normalize());
+        var actual = Differ.generate("src/test/resources/test3.json", "src/test/resources/test4.json", "plain");
+        String expected = Files.readString(Paths.get("src/test/resources/outputPlain.txt").toAbsolutePath().normalize());
         assertEquals(expected, actual);
     }
-
+    @Test
+    public void testYmlJson() throws Exception {
+        var actual = Differ.generate("src/test/resources/test1.yml", "src/test/resources/test2.yml", "json");
+        String expected = Files.readString(Paths.get("src/test/resources/outputJson.json").toAbsolutePath().normalize());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testJsonJson() throws Exception {
+        var actual = Differ.generate("src/test/resources/test3.json", "src/test/resources/test4.json", "json");
+        String expected = Files.readString(Paths.get("src/test/resources/outputJson.json").toAbsolutePath().normalize());
+        assertEquals(expected, actual);
+    }
 }
 
