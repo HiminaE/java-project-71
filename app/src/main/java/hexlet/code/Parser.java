715836c6eb3b9d9e9fabfd5570550ce6daf17c8b
HiminaE <himinaev@yandex.ru>
2024-05-10 14:13:34 +0300

    correct Parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -11,22 +11,22 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parsingYml(String fileContent) throws JsonProcessingException {
+    public static Map<String, Object> parsingYml(String content) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        return mapper.readValue(fileContent, new TypeReference<Map<String, Object>>() { });
+        return mapper.readValue(content, new TypeReference<Map<String, Object>>() { });
     }
-    public static Map<String, Object> parsingJson(String fileContent) throws IOException {
+    public static Map<String, Object> parsingJson(String content) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        LinkedHashMap<String, Object> linkedMap = mapper.readValue(fileContent,
+        LinkedHashMap<String, Object> linkedMap = mapper.readValue(content,
                 new TypeReference<LinkedHashMap<String, Object>>() { });
         return new LinkedHashMap<>(linkedMap);
     }
-    public static Map<String, Object> parser(String fileContent, String format) throws IOException {
+    public static Map<String, Object> parser(String content, String format) throws IOException {
         Map<String, Object> result = new HashMap<>();
         if (format.equals("json")) {
-            result = parsingJson(fileContent);
+            result = parsingJson(content);
         } else if (format.equals("yml") || format.equals("yaml")) {
-            result = parsingYml(fileContent);
+            result = parsingYml(content);
         } else {
             throw new RuntimeException("Неверный формат: " + format);
         }
