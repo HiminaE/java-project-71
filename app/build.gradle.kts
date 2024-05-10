8016f9eb9a90c76d63b211cd25d2dbae82ae5c57
HiminaE <himinaev@yandex.ru>
2024-05-10 10:35:14 +0300

    Correct build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     application
     jacoco
@@ -12,11 +15,31 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStackTraces = true
+        showCauses = true
+        showStandardStreams = true
+    }
+}
 
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
 dependencies {
-    implementation("info.picocli:picocli:4.7.5")
+    testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
-    testImplementation(dependencyNotation = "org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation ("info.picocli:picocli:4.7.6")
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0-rc1")
 }
@@ -25,6 +48,11 @@ dependencies {
 //        languageVersion = JavaLanguageVersion.of(21)
 //    }
 //}
-tasks.test {
-    //useJUnitPlatform()
-}
+//dependencies {
+//    implementation("info.picocli:picocli:4.7.5")
+//    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
+//    testImplementation(dependencyNotation = "org.junit.jupiter:junit-jupiter")
+//    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+//    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0-rc1")
+//}
+
