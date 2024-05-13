e5c821b600d66a4b5171ccb36f8ba38a9bb168d7
HiminaE <himinaev@yandex.ru>
2024-05-13 20:57:25 +0300

    correct Parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,35 +1,32 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parsingYml(String content) throws JsonProcessingException {
+    public static Map<String, Object> parsingYml(String content) throws Exception {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        return mapper.readValue(content, new TypeReference<Map<String, Object>>() { });
+        return mapper.readValue(content, Map.class);
     }
-    public static Map<String, Object> parsingJson(String content) throws IOException {
+    public static Map<String, Object> parsingJson(String content) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         LinkedHashMap<String, Object> linkedMap = mapper.readValue(content,
                 new TypeReference<LinkedHashMap<String, Object>>() { });
         return new LinkedHashMap<>(linkedMap);
     }
-    public static Map<String, Object> parser(String content, String format) throws IOException {
-        Map<String, Object> result = new HashMap<>();
-        if (format.equals("json")) {
-            result = parsingJson(content);
-        } else if (format.equals("yml") || format.equals("yaml")) {
-            result = parsingYml(content);
-        } else {
-            throw new RuntimeException("Неверный формат: " + format);
+    public static Map<String, Object> parser(String content, String format) throws Exception {
+        switch (format) {
+            case "yml":
+            case "yaml":
+                return parsingYml(content);
+            case "json":
+                return parsingJson(content);
+            default:
+                throw new Exception("Unknown format: '" + format + "'");
         }
-        return result;
     }
 }
