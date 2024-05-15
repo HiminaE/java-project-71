8041c406e254abe547c62f0de2918a12b2e3238d
HiminaE <himinaev@yandex.ru>
2024-05-15 07:15:07 +0300

    correct Builder

--- app/src/main/java/hexlet/code/Builder.java
+++ app/src/main/java/hexlet/code/Builder.java
@@ -9,32 +9,32 @@ import java.util.Map;
 import java.util.LinkedHashMap;
 
 public class Builder {
-    public static List<Map<String, Object>> build(Map<String, Object> file1, Map<String, Object> file2) {
-        Set<String> keys = new TreeSet<>(file1.keySet());
-        keys.addAll(file2.keySet());
+    public static List<Map<String, Object>> build(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
         List<Map<String, Object>> result = new LinkedList<>();
         for (var key :keys) {
-            Object value1 = file1.get(key) == null ? "null" : file1.get(key);
-            Object value2 = file2.get(key) == null ? "null" : file2.get(key);
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
             if (Objects.equals(value1, value2)) {
                 Map<String, Object> node = new LinkedHashMap<>();
                 node.put("type", "unchanged");
                 node.put("key", key);
                 node.put("newValue", value1);
                 result.add(node);
-            } else if (file1.containsKey(key) && !file2.containsKey(key)) {
+            } else if (data1.containsKey(key) && !data2.containsKey(key)) {
                 Map<String, Object> node = new LinkedHashMap<>();
                 node.put("type", "deleted");
                 node.put("key", key);
                 node.put("newValue", value1);
                 result.add(node);
-            } else if (file2.containsKey(key) && !file1.containsKey(key)) {
+            } else if (data2.containsKey(key) && !data1.containsKey(key)) {
                 Map<String, Object> node = new LinkedHashMap<>();
                 node.put("type", "added");
                 node.put("key", key);
                 node.put("newValue", value2);
                 result.add(node);
-            } else if (!Objects.equals(value1, value2)) {
+            } else if (!isEqual(data1.get(key), data2.get(key))) {
                 Map<String, Object> node = new LinkedHashMap<>();
                 node.put("type", "changed");
                 node.put("key", key);
@@ -46,5 +46,11 @@ public class Builder {
             }
         }
         return result;
+    }
+    private static boolean isEqual(Object value1, Object value2) {
+        if (value1 == null || value2 == null) {
+            return value1 == value2;
+        }
+        return value1.equals(value2);
     }
 }
