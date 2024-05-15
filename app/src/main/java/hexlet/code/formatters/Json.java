cbdc10e2ed1f7ca714011b893bed10d13f959142
HiminaE <himinaev@yandex.ru>
2024-05-15 07:57:19 +0300

    correct formatter

---
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -0,0 +1,14 @@
+package hexlet.code.formatters;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Json {
+    public static String jsonFormat(List<Map<String, Object>> diff) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(diff);
+    }
+}
