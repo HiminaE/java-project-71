12cf0597c1c0449267f93c9218938c4b494097af
HiminaE <himinaev@yandex.ru>
2024-05-15 07:43:10 +0300

    correct Plain

--- app/src/main/java/hexlet/code/formaters/Plain.java
+++ app/src/main/java/hexlet/code/formaters/Plain.java
@@ -1,4 +1,4 @@
-package hexlet.code.formaters;
+package hexlet.code.formatters;
 
 import java.util.List;
 import java.util.Map;
@@ -20,19 +20,21 @@ public class Plain {
                         .append(convertedValue(d.get("newValue"))).append("\n");
             } else if (d.get("type").equals("unchanged")) {
                 result.append("");
+            } else {
+                throw new RuntimeException("Unknown status: " + d.get("type"));
             }
         }
         return result.toString().trim();
     }
-    
+
     public static String convertedValue(Object value) {
         if (value instanceof Object[] || value instanceof Collections || value instanceof Map
                 || value instanceof ArrayList<?>) {
             return "[complex value]";
-        } else if (value instanceof String) {
-            return "'" + value + "'";
-        } else if (value == null) {
+        } else if (value.equals("null")) {
             return "null";
+        } else if (value instanceof  String) {
+            return "'" + value + "'";
         }
         return value.toString();
     }
