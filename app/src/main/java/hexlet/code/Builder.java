4459514a696f6faed84cc8a1d7ff63e50ab13f60
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-05-16 10:11:11 +0300

    Update Builder.java

--- app/src/main/java/hexlet/code/Builder.java
+++ app/src/main/java/hexlet/code/Builder.java
@@ -16,13 +16,7 @@ public class Builder {
         for (var key :keys) {
             Object value1 = data1.get(key);
             Object value2 = data2.get(key);
-            if (Objects.equals(value1, value2)) {
-                Map<String, Object> node = new LinkedHashMap<>();
-                node.put("type", "unchanged");
-                node.put("key", key);
-                node.put("newValue", value1);
-                result.add(node);
-            } else if (data1.containsKey(key) && !data2.containsKey(key)) {
+            if (data1.containsKey(key) && !data2.containsKey(key)) {
                 Map<String, Object> node = new LinkedHashMap<>();
                 node.put("type", "deleted");
                 node.put("key", key);
@@ -34,23 +28,23 @@ public class Builder {
                 node.put("key", key);
                 node.put("newValue", value2);
                 result.add(node);
-            } else if (!isEqual(value1, value2)) {
+            } else if (!Object.equals(value1, value2)) {
                 Map<String, Object> node = new LinkedHashMap<>();
                 node.put("type", "changed");
                 node.put("key", key);
                 node.put("oldValue", value1);
                 node.put("newValue", value2);
                 result.add(node);
+            } else if (Object.equals(value1, value2)) {
+                Map<String, Object> node = new LinkedHashMap<>();
+                node.put("type", "unchanged");
+                node.put("key", key);
+                node.put("newValue", value1);
+                result.add(node);
             } else {
-                throw new RuntimeException("Unknown status: " + key);
-            }
+                throw new RuntimeException("Unknown type: " + key);
+            }            
         }
         return result;
-    }
-    private static boolean isEqual(Object value1, Object value2) {
-        if (value1 == null || value2 == null) {
-            return value1 == value2;
-        }
-        return value1.equals(value2);
-    }
+    }    
 }
