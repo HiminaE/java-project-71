6a4ffdfa64ad568c789473d60142f57141f91e56
HiminaE <himinaev@yandex.ru>
2024-05-17 06:21:45 +0300

    correct Builder

--- app/src/main/java/hexlet/code/Builder.java
+++ app/src/main/java/hexlet/code/Builder.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,40 +12,39 @@ public class Builder {
     public static List<Map<String, Object>> build(Map<String, Object> data1, Map<String, Object> data2) {
         Set<String> keys = new TreeSet<>(data1.keySet());
         keys.addAll(data2.keySet());
-        List<Map<String, Object>> result = new ArrayList<>();
+        List<Map<String, Object>> result = new LinkedList<>();
         for (var key :keys) {
-            //Object value1 = data1.get(key);
-            //Object value2 = data2.get(key);
-            Map<String, Object> data = new LinkedHashMap<>();
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
+            //Map<String, Object> data = new LinkedHashMap<>();
             if (data1.containsKey(key) && !data2.containsKey(key)) {
-                //Map<String, Object> node = new LinkedHashMap<>();
-                data.put("type", "deleted");
-                data.put("key", key);
-                data.put("newValue", data1.get(key));
-                //result.add(node);
+                Map<String, Object> node = new LinkedHashMap<>();
+                node.put("type", "deleted");
+                node.put("key", key);
+                node.put("newValue", data1.get(key));
+                result.add(node);
             } else if (data2.containsKey(key) && !data1.containsKey(key)) {
-                //Map<String, Object> node = new LinkedHashMap<>();
-                data.put("type", "added");
-                data.put("key", key);
-                data.put("newValue", data2.get(key));
-                //result.add(node);
+                Map<String, Object> node = new LinkedHashMap<>();
+                node.put("type", "added");
+                node.put("key", key);
+                node.put("newValue", data2.get(key));
+                result.add(node);
             } else if (!Objects.equals(data1.get(key), data2.get(key))) {
-                //Map<String, Object> node = new LinkedHashMap<>();
-                data.put("type", "changed");
-                data.put("key", key);
-                data.put("oldValue", data1.get(key));
-                data.put("newValue", data2.get(key));
-                //result.add(node);
+                Map<String, Object> node = new LinkedHashMap<>();
+                node.put("type", "changed");
+                node.put("key", key);
+                node.put("oldValue", data1.get(key));
+                node.put("newValue", data2.get(key));
+                result.add(node);
             } else if (Objects.equals(data1.get(key), data2.get(key))) {
-                //Map<String, Object> node = new LinkedHashMap<>();
-                data.put("type", "unchanged");
-                data.put("key", key);
-                data.put("newValue", data1.get(key));
-                //result.add(node);
+                Map<String, Object> node = new LinkedHashMap<>();
+                node.put("type", "unchanged");
+                node.put("key", key);
+                node.put("newValue", data1.get(key));
+                result.add(node);
             } else {
                 throw new RuntimeException("Unknown type: " + key);
             }
-            result.add(data);
         }
         return result;
     }
