5328a76be4dabe76c1f0ba8967c3ed087c8112cb
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-05-17 12:30:25 +0300

    Update Json.java

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -9,6 +9,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Json {
     public static String jsonFormat(List<Map<String, Object>> diff) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(diff);
+        return objectMapper.writeValueAsString(diff).trim();
     }
 }
